5b27ff76d3cb3d83f01607d4690a2e806e90d550
Abir <samiulabir41@gmail.com>
2024-07-04 01:47:18 +0600

    updated slider imgs

--- src/Data/slider.js
+++ src/Data/slider.js
@@ -1,47 +1,52 @@
-import sampleImg from '../pictures/slider-pizza.png';
+import sampleImg1 from '../pictures/slider-pizza.png';
+import sampleImg2 from '../pictures/6.png';
+import sampleImg3 from '../pictures/3.png';
+import sampleImg4 from '../pictures/4.png';
+import sampleImg5 from '../pictures/5.png';
+import sampleImg6 from '../pictures/2.png';
 
 const sliderData = [
   {
     id: 1,
-    img: sampleImg,
-    name: 'Classic Pizza 1',
-    price: '$13',
+    img: sampleImg1,
+    name: 'Classic',
+    price: '$29',
     desc: 'Classic Pizza Features Tangy Tomato Sauce, Gooey Mozzarella Cheese, And Flavorful Toppings On Crispy Crust.',
   },
   {
     id: 2,
-    img: sampleImg,
-    name: 'Classic Pizza 2',
-    price: '$13',
-    desc: 'Classic Pizza Features Tangy Tomato Sauce, Gooey Mozzarella Cheese, And Flavorful Toppings On Crispy Crust.',
+    img: sampleImg2,
+    name: 'Pepperoni',
+    price: '$39.99',
+    desc: 'A pepperoni pizza boasts spicy pepperoni slices, melted mozzarella, and savory tomato sauce on a crispy crust.',
   },
   {
     id: 3,
-    img: sampleImg,
-    name: 'Classic Pizza 3',
-    price: '$13',
-    desc: 'Classic Pizza Features Tangy Tomato Sauce, Gooey Mozzarella Cheese, And Flavorful Toppings On Crispy Crust.',
+    img: sampleImg3,
+    name: 'Extra chezzy',
+    price: '$54',
+    desc: 'A cheesy pizza boasts layers of melted mozzarella, gooey cheddar, pepperoni slices, and rich Parmesan on a crisp crust.',
   },
   {
     id: 4,
-    img: sampleImg,
-    name: 'Classic Pizza 4',
-    price: '$13',
-    desc: 'Classic Pizza Features Tangy Tomato Sauce, Gooey Mozzarella Cheese, And Flavorful Toppings On Crispy Crust.',
+    img: sampleImg4,
+    name: 'Vegan',
+    price: '$40',
+    desc: 'A vegan pizza has plant-based cheese, fresh veggies, savory tomato sauce, and a crispy, dairy-free crust.',
   },
   {
     id: 5,
-    img: sampleImg,
-    name: 'Classic Pizza 5',
-    price: '$13',
-    desc: 'Classic Pizza Features Tangy Tomato Sauce, Gooey Mozzarella Cheese, And Flavorful Toppings On Crispy Crust.',
+    img: sampleImg5,
+    name: 'Mini beast',
+    price: '$27',
+    desc: 'A mini pizza offers more than  a bite-sized delight with tangy sauce, melted cheese, and choice toppings.',
   },
   {
     id: 6,
-    img: sampleImg,
-    name: 'Classic Pizza 6',
-    price: '$13',
-    desc: 'Classic Pizza Features Tangy Tomato Sauce, Gooey Mozzarella Cheese, And Flavorful Toppings On Crispy Crust.',
+    img: sampleImg6,
+    name: 'Regular',
+    price: '$30',
+    desc: 'A regular pizza has pure perfection and a perfect balance of sauce, melted cheese, and various savory toppings.',
   },
 ];
 
