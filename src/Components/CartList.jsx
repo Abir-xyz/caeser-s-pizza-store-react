0032d13657e63e8f13829bee5dc01cb4d8384d3f
Abir <samiulabir41@gmail.com>
2024-07-25 12:55:17 +0600

    added cart empty warning

--- src/Components/CartList.jsx
+++ src/Components/CartList.jsx
@@ -6,35 +6,41 @@ const CartList = ({ items }) => {
 
   return (
     <div className='cart-item'>
-      {items &&
-        items.map((item) => {
-          return (
-            <article className='cart-article' key={item.id}>
-              <div className='inner-cart-article'>
-                <div className='cart-img'>
-                  <img src={item.img} className='cart-img' />
-                </div>
-                <div className='cart-info'>
-                  <div className='cart-item-name'>
-                    <p>{item.name}</p>
+      {cartItems.length === 0 ? (
+        <p className='cart-warning'>your cart is empty</p>
+      ) : (
+        <div className='cartlist-header'>
+          {items &&
+            items.map((item) => {
+              return (
+                <article className='cart-article' key={item.id}>
+                  <div className='inner-cart-article'>
+                    <div className='cart-img'>
+                      <img src={item.img} className='cart-img' />
+                    </div>
+                    <div className='cart-info'>
+                      <div className='cart-item-name'>
+                        <p>{item.name}</p>
+                      </div>
+                      <div className='cart-item-quantity-wrapper'>
+                        <p className='cart-item-quantity'>{item.quantity} x</p>
+                        <p className='cart-item-price'>{item.price}</p>
+                      </div>
+                    </div>
+                    <div
+                      className='cart-item-delete cart-deleteBtn'
+                      onClick={() => removeItemFromCart(item.id)}
+                    >
+                      <span>
+                        <i className='fa-solid fa-trash'></i>
+                      </span>
+                    </div>
                   </div>
-                  <div className='cart-item-quantity-wrapper'>
-                    <p className='cart-item-quantity'>{item.quantity} x</p>
-                    <p className='cart-item-price'>{item.price}</p>
-                  </div>
-                </div>
-                <div
-                  className='cart-item-delete cart-deleteBtn'
-                  onClick={() => removeItemFromCart(item.id)}
-                >
-                  <span>
-                    <i className='fa-solid fa-trash'></i>
-                  </span>
-                </div>
-              </div>
-            </article>
-          );
-        })}
+                </article>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 };
